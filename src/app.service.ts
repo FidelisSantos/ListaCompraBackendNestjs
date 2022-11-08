a4512e68c72fea3b863956806da1084cc36814f3
FidelisSantos <vitorfidelisantos@gmail.com>
2022-11-08 20:53:13 -0300

    Arrumando um erro

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,10 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-<<<<<<< HEAD
     return 'Online';
-=======
-    return 'Hello World!';
->>>>>>> d4a1cb7... Aplicação estável
   }
 }
