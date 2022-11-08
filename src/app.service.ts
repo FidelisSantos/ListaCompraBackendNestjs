8d4198a19d9bdcc6b90281ed3a8f16b82cbf959c
FidelisSantos <vitorfidelisantos@gmail.com>
2022-11-08 20:49:25 -0300

    Resolvendo pendencia

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+<<<<<<< HEAD
+    return 'Online';
+=======
     return 'Hello World!';
+>>>>>>> d4a1cb7... Aplicação estável
   }
 }
