42daf1dfd54af70aa071d7896b0f632719cf435d
FidelisSantos <vitorfidelisantos@gmail.com>
2022-11-08 20:37:56 -0300

    Retirando informações desnecessárias

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -25,7 +25,6 @@ export class AuthService {
   }
 
   async login(user: User) {
-    console.log(user);
     const payload = { email: user.email, sub: user.id, role: user.role };
 
     return { acess_token: this.jwtService.sign(payload) };
--- src/user/types/user.view.ts
+++ src/user/types/user.view.ts
@@ -1,7 +1,6 @@
 export class ViewUser {
   id: string;
   username: string;
-  token: string;
   constructor(id: string, username: string) {
     this.id = id;
     this.username = username;
